0ac5b9b279f12de18658bfbb7f86ca6032e8ee57
jiangjiwei <jiangjiwei0322@outlook.com>
2020-04-12 18:22:45 +0800

    refactor: 准备向外回调允许的权限和不允许的权限

--- coroutinespermisstions/src/main/java/com/dj/coroutines/permisstions/InlinePermissionResult.kt
+++ coroutinespermisstions/src/main/java/com/dj/coroutines/permisstions/InlinePermissionResult.kt
@@ -1,5 +1,7 @@
 package com.dj.coroutines.permisstions
 
+import android.content.pm.PackageManager
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.dj.coroutines.permisstions.callbacks.FailCallback
@@ -13,18 +15,23 @@ import java.lang.ref.WeakReference
 
 class InlinePermissionResult {
     private val TAG = "ACTIVITY_RESULT_FRAGMENT_WEEEEE"
+    private val tag = "InlinePermissionResult"
     private var activityReference: Reference<FragmentActivity>
     private val successCallbacks=ArrayList<SuccessCallback>()
     private val failCallbacks=ArrayList<FailCallback>()
     private val responseListeners=ArrayList<RequestResultListener>()
     private var listener=object : RequestPermissionFragment.RequestPermissionsListener{
+        val resultGrantedPermissionArray = mutableListOf<String>()
+        val resultDeniedPermissionArray = mutableListOf<String>()
         override fun onRequestPermissions(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
             //如果grantResults中，0代表该权限请求成功，-1代表失败
             var isRequestSuccess=true
+            Log.d(tag, " permissions size is ${permissions.size} content is  ${permissions} grantResults size is ${grantResults.size} content is $grantResults")
             for (code in grantResults){
-                if (code==-1) {
-                    isRequestSuccess=false
-                    break
+                if (PackageManager.PERMISSION_DENIED == code) {
+                    resultDeniedPermissionArray.add(permissions[grantResults.indexOf(code)])
+                } else {
+                    resultGrantedPermissionArray.add(permissions[grantResults.indexOf(code)])
                 }
             }
             if (isRequestSuccess){
