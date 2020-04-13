1f1818cd6ad93a672d6bad0f61278d9060487966
jiangjiwei <jiangjiwei0322@outlook.com>
2020-04-13 10:57:00 +0800

    feat: 新增在 Activity 下使用的示例代码

---
+++ app/src/main/java/com/dj/request/PermissionActivity.kt
@@ -0,0 +1,50 @@
+package com.dj.request
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.dj.coroutines.permisstions.requestPermissionsForResult
+import kotlinx.android.synthetic.main.activity_permission.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * @description
+ * @author: Created jiangjiwei in 2020/4/13 10:32
+ */
+class PermissionActivity : AppCompatActivity() {
+    private val permissions =
+        arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    companion object {
+        const val tag = "PermissionActivity"
+
+        fun start(context: Context) {
+            val stater = Intent(context, PermissionActivity::class.java)
+            context.startActivity(stater)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+
+        btnActRequire.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                requestPermissionsForResult(
+                    fun(allGranted: Boolean, grantedList: MutableList<String>) {
+                        Log.d(tag, "allGranted: $allGranted grantedList: $grantedList")
+                    },
+                    fun(allDenied: Boolean, deniedList: MutableList<String>) {
+                        Log.d(tag, "allDenied: $allDenied deniedList: $deniedList")
+                    },
+                    *permissions
+                )
+            }
+        }
+    }
+}
