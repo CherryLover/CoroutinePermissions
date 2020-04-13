18fff629e1ee4f5fda13f4b711cb2330b9056419
jiangjiwei <jiangjiwei0322@outlook.com>
2020-04-13 10:56:28 +0800

    refactor: 示例代码更新

--- app/src/main/java/com/dj/request/MainActivity.kt
+++ app/src/main/java/com/dj/request/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.dj.request
 
-import android.Manifest
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction().replace(R.id.con,PermissionFragment()).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menuActivity) {
+            PermissionActivity.start(this)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/dj/request/PermissionFragment.kt
+++ app/src/main/java/com/dj/request/PermissionFragment.kt
@@ -2,12 +2,11 @@ package com.dj.request
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.dj.coroutines.permisstions.InlineRequestPermissionException
 import com.dj.coroutines.permisstions.requestPermissionsForResult
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,6 +14,11 @@ import kotlinx.coroutines.launch
 
 class PermissionFragment:Fragment(){
     private val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    companion object {
+        const val logt = "PermissionFragment"
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,12 +30,15 @@ class PermissionFragment:Fragment(){
     }
     private fun initView(){
         CoroutineScope(Dispatchers.Main).launch {
-            try {
-                requestPermissionsForResult(*permissions)
-                Toast.makeText(activity,"权限请求成功", Toast.LENGTH_SHORT).show()
-            }catch (e: InlineRequestPermissionException){
-                Toast.makeText(activity,"权限请求失败", Toast.LENGTH_SHORT).show()
-            }
+            requestPermissionsForResult(
+                fun(allGranted: Boolean, grantedList: MutableList<String>) {
+                    Log.d(logt, "allGranted: $allGranted grantedList: $grantedList")
+                },
+                fun(allDenied: Boolean, deniedList: MutableList<String>) {
+                    Log.d(logt, "allDenied: $allDenied deniedList: $deniedList")
+                },
+                *permissions
+            )
         }
     }
 }
